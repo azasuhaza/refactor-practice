c8fcd64376d6e640c92c39e5b19f64a34ae45fdc
azasuhaza <azasuhaza@gmail.com>
2024-12-22 23:59:44 +0800

    initial check-in

---
+++ src/main/java/com/aza/suhaza/refactor/util/WikiPagePath.java
@@ -0,0 +1,10 @@
+package com.aza.suhaza.refactor.util;
+
+public class WikiPagePath {
+
+  public WikiPagePath getFullPath(WikiPage suiteSetup) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
